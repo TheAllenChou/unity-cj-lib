571076aa4a0d67e41c98571b41efce702ab344e0
Ming-Lun Allen Chou <minglun.chou@gmail.com>
2018-05-27 17:35:02 -0700

    Simplex noise gradient with custom samples.

--- Unity CJ Lib/Assets/CjLib/Noise/SimplexNoiseGradient.cs
+++ Unity CJ Lib/Assets/CjLib/Noise/SimplexNoiseGradient.cs
@@ -15,10 +15,15 @@ namespace CjLib
 {
   public class SimplexNoiseGradient
   {
+    // common
+    //-------------------------------------------------------------------------
+
     private static bool s_simplexGradientInit = false;
     private static ComputeShader s_simplexGradient;
     private static int s_simplexGradient2KernelId;
     private static int s_simplexGradient3KernelId;
+    private static int s_simplexGradientGrid2KernelId;
+    private static int s_simplexGradientGrid3KernelId;
     private static void InitSimplex()
     {
       NoiseCommon.InitCsId();
@@ -29,27 +34,75 @@ namespace CjLib
       s_simplexGradient = (ComputeShader) Resources.Load("SimplexNoiseGradientCs");
       s_simplexGradient2KernelId = s_simplexGradient.FindKernel("SimplexGradient2");
       s_simplexGradient3KernelId = s_simplexGradient.FindKernel("SimplexGradient3");
+      s_simplexGradientGrid2KernelId = s_simplexGradient.FindKernel("SimplexGradientGrid2");
+      s_simplexGradientGrid3KernelId = s_simplexGradient.FindKernel("SimplexGradientGrid3");
     }
 
-    private static void GetSimplex2(out ComputeShader shader, out int kernelId)
+    private static void GetSimplexGradient2(out ComputeShader shader, out int kernelId)
     {
       InitSimplex();
       shader = s_simplexGradient;
       kernelId = s_simplexGradient2KernelId;
     }
 
-    private static void GetSimplex3(out ComputeShader shader, out int kernelId)
+    private static void GetSimplexGradient3(out ComputeShader shader, out int kernelId)
     {
       InitSimplex();
       shader = s_simplexGradient;
       kernelId = s_simplexGradient3KernelId;
     }
 
+    private static void GetSimplexGradientGrid2(out ComputeShader shader, out int kernelId)
+    {
+      InitSimplex();
+      shader = s_simplexGradient;
+      kernelId = s_simplexGradientGrid2KernelId;
+    }
+
+    private static void GetSimplexGradientGrid3(out ComputeShader shader, out int kernelId)
+    {
+      InitSimplex();
+      shader = s_simplexGradient;
+      kernelId = s_simplexGradientGrid3KernelId;
+    }
+
+    //-------------------------------------------------------------------------
+    // end: common
+
+
+    // GPU compute / custom sample points
+    //-------------------------------------------------------------------------
+
+    public static void Compute(Vector2[] input, Vector2[] output, float[] offset, int numOctaves, float octaveOffsetFactor)
+    {
+      ComputeShader shader;
+      int kernelId;
+      GetSimplexGradient2(out shader, out kernelId);
+      float[] aOffset = { offset[0], offset[1], 0.0f };
+      NoiseCommon.Compute(input, output, shader, kernelId, sizeof(float) * 2, aOffset, numOctaves, octaveOffsetFactor);
+    }
+
+    public static void Compute(Vector3[] input, Vector3[] output, float[] offset, int numOctaves, float octaveOffsetFactor)
+    {
+      ComputeShader shader;
+      int kernelId;
+      GetSimplexGradient3(out shader, out kernelId);
+      float[] aOffset = { offset[0], offset[1], offset[2] };
+      NoiseCommon.Compute(input, output, shader, kernelId, sizeof(float) * 3, aOffset, numOctaves, octaveOffsetFactor);
+    }
+
+    //-------------------------------------------------------------------------
+    // end: GPU compute / custom sample points
+
+
+    // GPU compute / grid sample points
+    //-------------------------------------------------------------------------
+
     public static void Compute(Vector2[] output, float scale, float offset, int numOctaves, float octaveOffsetFactor)
     {
       ComputeShader shader;
       int kernelId;
-      GetSimplex2(out shader, out kernelId);
+      GetSimplexGradientGrid2(out shader, out kernelId);
       int[] dimension = new int[] { output.GetLength(0), 1, 1 };
       float[] aScale = { scale, 1.0f, 1.0f };
       float[] aOffset = { offset, 0.0f, 0.0f };
@@ -60,7 +113,7 @@ namespace CjLib
     {
       ComputeShader shader;
       int kernelId;
-      GetSimplex2(out shader, out kernelId);
+      GetSimplexGradientGrid2(out shader, out kernelId);
       int[] dimension = new int[] { output.GetLength(0), output.GetLength(1), 1 };
       float[] aScale = { scale[0], scale[1], 1.0f };
       float[] aOffset = { offset[0], offset[1], 0.0f };
@@ -71,7 +124,7 @@ namespace CjLib
     {
       ComputeShader shader;
       int kernelId;
-      GetSimplex3(out shader, out kernelId);
+      GetSimplexGradientGrid3(out shader, out kernelId);
       int[] dimension = new int[] { output.GetLength(0), 1, 1 };
       float[] aScale = { scale, 1.0f, 1.0f };
       float[] aOffset = { offset, 0.0f, 0.0f };
@@ -82,7 +135,7 @@ namespace CjLib
     {
       ComputeShader shader;
       int kernelId;
-      GetSimplex3(out shader, out kernelId);
+      GetSimplexGradientGrid3(out shader, out kernelId);
       int[] dimension = new int[] { output.GetLength(0), output.GetLength(1), 1 };
       float[] aScale = { scale[0], scale[1], 1.0f };
       float[] aOffset = { offset[0], offset[1], 0.0f };
@@ -93,11 +146,14 @@ namespace CjLib
     {
       ComputeShader shader;
       int kernelId;
-      GetSimplex3(out shader, out kernelId);
+      GetSimplexGradientGrid3(out shader, out kernelId);
       int[] dimension = new int[] { output.GetLength(0), output.GetLength(1), output.GetLength(2) };
       float[] aScale = { scale[0], scale[1], scale[2] };
       float[] aOffset = { offset[0], offset[1], offset[2] };
       NoiseCommon.Compute(output, shader, kernelId, dimension, sizeof(float) * 3, aScale, aOffset, numOctaves, octaveOffsetFactor);
     }
+
+    //-------------------------------------------------------------------------
+    // end: GPU compute / grid sample points
   }
 }
