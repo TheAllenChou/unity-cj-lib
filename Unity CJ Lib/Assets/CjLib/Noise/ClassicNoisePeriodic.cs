3b78e7dfa3892a4c18aa3a0b34b93f131c50b4b1
Ming-Lun Allen Chou <minglun.chou@gmail.com>
2018-05-28 16:00:45 -0700

    GPU compute class Perlin noise with custom samples.

--- Unity CJ Lib/Assets/CjLib/Noise/ClassicNoisePeriodic.cs
+++ Unity CJ Lib/Assets/CjLib/Noise/ClassicNoisePeriodic.cs
@@ -15,41 +15,69 @@ namespace CjLib
 {
   public class ClassicNoisePeriodic
   {
-    private static bool s_simplexInit = false;
-    private static ComputeShader s_simplex;
-    private static int s_simplex2KernelId;
-    private static int s_simplex3KernelId;
+    // common
+    //-------------------------------------------------------------------------
+
+    private static bool s_classicPeriodicInit = false;
+    private static ComputeShader s_classicPeriodic;
+    private static int s_classicPeriodicGrid2KernelId;
+    private static int s_classicPeriodicGrid3KernelId;
+    private static int s_classicPeriodicCustom2KernelId;
+    private static int s_classicPeriodicCustom3KernelId;
     private static void InitClassicPeriodic()
     {
       NoiseCommon.InitCsId();
 
-      if (s_simplexInit)
+      if (s_classicPeriodicInit)
         return;
 
-      s_simplex = (ComputeShader) Resources.Load("ClassicNoisePeriodicCs");
-      s_simplex2KernelId = s_simplex.FindKernel("ClassicPeriodic2");
-      s_simplex3KernelId = s_simplex.FindKernel("ClassicPeriodic3");
+      s_classicPeriodic = (ComputeShader) Resources.Load("ClassicNoisePeriodicCs");
+      s_classicPeriodicGrid2KernelId = s_classicPeriodic.FindKernel("ClassicPeriodicGrid2");
+      s_classicPeriodicGrid3KernelId = s_classicPeriodic.FindKernel("ClassicPeriodicGrid3");
+      s_classicPeriodicCustom2KernelId = s_classicPeriodic.FindKernel("ClassicPeriodicGrid2");
+      s_classicPeriodicCustom3KernelId = s_classicPeriodic.FindKernel("ClassicPeriodicGrid3");
     }
 
-    private static void GetClassicPeriodic2(out ComputeShader shader, out int kernelId)
+    private static void GetClassicPeriodicGrid2(out ComputeShader shader, out int kernelId)
     {
       InitClassicPeriodic();
-      shader = s_simplex;
-      kernelId = s_simplex2KernelId;
+      shader = s_classicPeriodic;
+      kernelId = s_classicPeriodicGrid2KernelId;
     }
 
-    private static void GetClassicPeriodic3(out ComputeShader shader, out int kernelId)
+    private static void GetClassicPeriodicGrid3(out ComputeShader shader, out int kernelId)
     {
       InitClassicPeriodic();
-      shader = s_simplex;
-      kernelId = s_simplex3KernelId;
+      shader = s_classicPeriodic;
+      kernelId = s_classicPeriodicGrid3KernelId;
     }
+
+    private static void GetClassicPeriodicCustom2(out ComputeShader shader, out int kernelId)
+    {
+      InitClassicPeriodic();
+      shader = s_classicPeriodic;
+      kernelId = s_classicPeriodicCustom2KernelId;
+    }
+
+    private static void GetClassicPeriodicCustom3(out ComputeShader shader, out int kernelId)
+    {
+      InitClassicPeriodic();
+      shader = s_classicPeriodic;
+      kernelId = s_classicPeriodicCustom3KernelId;
+    }
+
+    //-------------------------------------------------------------------------
+    // end: common
+
+
+    // GPU compute / grid samples
+    //-------------------------------------------------------------------------
 
     public static void Compute(float[] output, float scale, float offset, float period, int numOctaves, float octaveOffsetFactor)
     {
       ComputeShader shader;
       int kernelId;
-      GetClassicPeriodic2(out shader, out kernelId);
+      GetClassicPeriodicGrid2(out shader, out kernelId);
       int[] dimension = new int[] { output.GetLength(0), 1, 1 };
       float[] aScale = { scale, 1.0f, 1.0f };
       float[] aOffset = { offset, 0.0f, 0.0f };
@@ -61,7 +89,7 @@ namespace CjLib
     {
       ComputeShader shader;
       int kernelId;
-      GetClassicPeriodic2(out shader, out kernelId);
+      GetClassicPeriodicGrid2(out shader, out kernelId);
       int[] dimension = new int[] { output.GetLength(0), output.GetLength(1), 1 };
       float[] aScale = { scale[0], scale[1], 1.0f };
       float[] aOffset = { offset[0], offset[1], 0.0f };
@@ -73,12 +101,42 @@ namespace CjLib
     {
       ComputeShader shader;
       int kernelId;
-      GetClassicPeriodic3(out shader, out kernelId);
+      GetClassicPeriodicGrid3(out shader, out kernelId);
       int[] dimension = new int[] { output.GetLength(0), output.GetLength(1), output.GetLength(2) };
       float[] aScale = { scale[0], scale[1], scale[2] };
       float[] aOffset = { offset[0], offset[1], offset[2] };
       float[] aPeriod = { period[0], period[1], period[2] };
       NoiseCommon.Compute(output, shader, kernelId, dimension, aScale, aOffset, aPeriod, numOctaves, octaveOffsetFactor);
     }
+
+    //-------------------------------------------------------------------------
+    // end: GPU compute / grid samples
+
+
+    // GPU compute / custom samples
+    //-------------------------------------------------------------------------
+
+    public static void Compute(Vector2[] input, float[] output, float[] scale, float[] offset, float[] period, int numOctaves, float octaveOffsetFactor)
+    {
+      ComputeShader shader;
+      int kernelId;
+      GetClassicPeriodicCustom2(out shader, out kernelId);
+      float[] aScale = { scale[0], scale[1], 1.0f };
+      float[] aOffset = { offset[0], offset[1], 0.0f };
+      NoiseCommon.Compute(input, output, shader, kernelId, aScale, aOffset, period, numOctaves, octaveOffsetFactor);
+    }
+
+    public static void Compute(Vector3[] input, float[] output, float[] scale, float[] offset, float[] period, int numOctaves, float octaveOffsetFactor)
+    {
+      ComputeShader shader;
+      int kernelId;
+      GetClassicPeriodicCustom3(out shader, out kernelId);
+      float[] aScale = { scale[0], scale[1], scale[2] };
+      float[] aOffset = { offset[0], offset[1], offset[2] };
+      NoiseCommon.Compute(input, output, shader, kernelId, aScale, aOffset, period, numOctaves, octaveOffsetFactor);
+    }
+
+    //-------------------------------------------------------------------------
+    // end: GPU compute / custom samples
   }
 }
