db7a23cd787fd0c07d9d792e6b9c4df8a8bdfc7c
Allen Chou <minglun.chou@gmail.com>
2019-07-21 13:10:23 -0700

    Example.

---
+++ Unity CJ Lib/Assets/Example/Rotation Spring/QuaternionSpringTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+using CjLib;
+
+public class QuaternionSpringTest : MonoBehaviour
+{
+  public Transform RotationTarget;
+
+  private QuaternionSpring m_spring;
+
+  public void Start()
+  {
+    m_spring.Reset();
+  }
+
+  void Update()
+  {
+    transform.rotation = m_spring.TrackDampingRatio(RotationTarget.rotation, 20.0f, 0.2f, Time.deltaTime);
+  }
+}
