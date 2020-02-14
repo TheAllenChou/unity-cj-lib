468ba00be7c1dfcbced8db861edebc760bb348fc
Allen Chou <cjcat2266@gmail.com>
2020-02-14 15:29:22 +0800

    Refactor .

--- Unity CJ Lib/Assets/CjLib/Script/Math/QuaternionUtil.cs
+++ Unity CJ Lib/Assets/CjLib/Script/Math/QuaternionUtil.cs
@@ -94,9 +94,8 @@ namespace CjLib
     // https://gafferongames.com/post/physics_in_3d/
     public static Quaternion Integrate(Quaternion q, Vector3 omega, float dt)
     {
-      omega *= 0.5f;
-      Quaternion p = (new Quaternion(omega.x, omega.y, omega.z, 0.0f)) * q;
-      return Normalize(new Quaternion(q.x + p.x * dt, q.y + p.y * dt, q.z + p.z * dt, q.w + p.w * dt));
+      dt *= 0.5f;
+      return Normalize(new Quaternion(omega.x * dt, omega.y * dt, omega.z * dt, 1.0f) * q);
     }
 
     public static Vector4 ToVector4(Quaternion q)
